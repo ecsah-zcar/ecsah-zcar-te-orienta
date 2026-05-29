f657ba38bfec8ce72c15a61b49dacb1f5196c7a4
candradelugo <candradelugo@gmail.com>
2026-05-29 15:33:19 -0500

    script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ const contenidoEstudiantes = [
         titulo: "🔄 Cambios de malla curricular",
         descripcion: "Resoluciones, planes de transición y equivalencias entre mallas antiguas y nuevas.",
         enlaces: [
-            { texto: "📄 Cursos sin oferta y sus equivalencias Psicología 16-04", ruta: "documentos/Equivalencias-3443-para-1604.pdf" },
+            { texto: "📄 Cursos sin oferta y sus equivalencias Psicología 16-04", ruta: "documentos/Equivalencias-3443-para-16-04.pdf" },
             { texto: "⚖️ Resolución vigente", ruta: "#" }
         ]
     },
