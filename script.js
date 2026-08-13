1ffd2b8c3f94decdae77529a9fd1cd88e8b3622a
candradelugo <candradelugo@gmail.com>
2026-08-13 10:33:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const contenidoEstudiantes = [
     descripcion: "Toda la información sobre el proceso de matrícula: fechas, requisitos, pasos y documentación necesaria.",
     enlaces: [
         { texto: "📅 Calendario Académico 2026", ruta: "documentos/Calendario-Académico-2026.pdf" },
-        { texto: "📋 Oferta Académica 16-04", ruta: "documentos/Cursos-Ofertados-ECSAH-Periodo-1604.pdf" }
+        { texto: "📋 Oferta Académica 16-05", ruta: "documentos/Oferta_Academica_ECSAH_16-05_2026.pdf" }
     ],
     desplegable: {
         titulo: "Planea tu matrícula",
